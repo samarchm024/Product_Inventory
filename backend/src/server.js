2427be34f46ce4376dfe114a9d5fcd56c7aa7216
samar gound <110964525+Rgound111@users.noreply.github.com>
2026-03-03 11:01:08 +0530

    added frontend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://product-inventoryfrontend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
